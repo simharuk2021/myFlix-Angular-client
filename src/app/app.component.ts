530a876976a07c2da0bcb0b804e33aa40b2e10e0
simharuk2021 <83026016+simharuk2021@users.noreply.github.com>
2022-01-21 14:58:16 +0000

    MOVIE CARD BUTTON AND COMPONENT ADDED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -17,13 +18,20 @@ export class AppComponent {
 openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
 // Assigning the dialog a width
-    width: '280px'
+    width: '480px'
     });
 }
+// This is the function that will open the dialog when the login button is clicked
 openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
 // Assigning the dialog a width
-    width: '280px'
+    width: '480px'
+    });
+  }
+//This is the function that will open the dialog when the All Movies button is clicked   
+openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+     width: '500px'
     });
   }
 }
